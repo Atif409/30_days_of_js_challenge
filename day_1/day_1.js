eb15bdb70ef12f83f2f3b0d2c3c0a28e78d4595d
Atif409 <afafatif88@gmail.com>
2024-07-13 09:00:59 +0500

    I have Started My 30 Days of challenge and also completed the challenges of Day_1.

---
+++ day_1/day_1.js
@@ -0,0 +1,36 @@
+var num = 5;
+console.log(num);
+let str = "Hi I am tarting 30 days of JavaScript challenge.";
+console.log(str);
+const bol = true;
+console.log(bol);
+
+// Creating an object
+const my_info = {
+    name: "Muhammad Atif",
+    Age: 21,
+    City: "Islamabad",
+};
+
+// Array
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Trying to findout our varibales types
+console.log(`Type of num is: ${typeof (num)}`);
+console.log(`Type of str is: ${typeof (str)}`);
+console.log(`Type of bol is: ${typeof (bol)}`);
+console.log(`Type of my_info is: ${typeof (my_info)}`);
+console.log(`Type of arr is: ${typeof (arr)}`);
+
+// creating new str variable
+let str2 = str;
+console.log(str);
+str2 = "That is my Day_1 of JS challenge.";
+console.log(str2);
+
+// Trying to re-assign const variable
+
+// bol = false;
+// console.log(bol);
+
+// TypeError: Assignment to Constant variable
