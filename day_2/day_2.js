7e8bacc139a0fab5cb877165769c737529554878
Atif409 <afafatif88@gmail.com>
2024-07-14 12:15:08 +0500

    Adding code file of  day_2 challenge solution

---
+++ day_2/day_2.js
@@ -0,0 +1,71 @@
+//  Activity.1
+let num1 = 15;
+let num2 = 10;
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multi = num1 * num2;
+let div = num1 / num2;
+
+console.log()
+console.log("====================================")
+console.log()
+
+console.log(`Sum of two number is: ${sum}`);
+console.log(`num1 - num2 is equal to: ${minus}`);
+console.log(`Multiplication of two number is: ${multi}`);
+console.log(`num1 / num2 is equal to: ${div}`);
+
+
+console.log()
+console.log("====================================")
+console.log()
+
+num1 +=4;
+num2 -=4;
+
+
+console.log(`The Result of num1 +=4 is: ${num1}`);
+console.log(`The Result of num2 -=4 is: ${num2}`);
+
+console.log()
+console.log("====================================")
+console.log()
+
+console.log(`The Result of num1>num2 is: ${num1>num2}`);
+console.log(`The Result of num1<num2 is: ${num1<num2}`);
+
+console.log()
+console.log("====================================")
+console.log()
+
+console.log(`The Result of num1>=num2 is: ${num1>=num2}`);
+console.log(`The Result of num1<=num2 is: ${num1<=num2}`);
+
+console.log()
+console.log("====================================")
+console.log()
+
+// Conditions 
+
+let result1 = num1>=4 && num2<=5;
+
+let result2 = num1<20 || num2<1;
+
+let result3 = !result1;
+console.log(result1)
+console.log(result2)
+console.log(result3)
+
+console.log()
+console.log("====================================")
+console.log()
+
+let num = 55;
+
+let checkPositive = num>0 ? true : false;
+
+console.log(checkPositive)
+
+console.log()
+console.log("====================================")
+console.log()
