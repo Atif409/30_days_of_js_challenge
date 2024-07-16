0bfa1592f7632542fb51dc867c5180690dd98d91
Atif409 <afafatif88@gmail.com>
2024-07-16 16:40:49 +0500

    Activity 3 tasks completed in this file.

---
+++ day_3/activity3.js
@@ -0,0 +1,58 @@
+/*
+let day ;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("You have not any number for a day!");
+
+}
+
+*/
+
+/*let marks = -55;
+let grade = 'hi';
+
+switch (true) {
+    case marks >= 90 && marks <= 100:
+        grade ="A";
+        break;
+        case marks >= 80 && marks < 90:
+        grade ="B";
+        break;
+        case marks >= 70 && marks <80:
+        grade ="C";
+        break;
+        case marks >= 60 && marks <70:
+        grade ="D";
+        break;
+        case marks <60 && marks >=0:
+        grade ="F";
+        break;
+        default:
+            grade = "Please enter valid marks"
+
+}
+console.log(grade);
+*/
+
