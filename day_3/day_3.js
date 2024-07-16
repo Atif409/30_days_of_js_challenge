e586f173e27e5bddbdbbca7d406a8faced5ea5bb
Atif409 <afafatif88@gmail.com>
2024-07-16 16:39:21 +0500

    Activity 1,2 and 3 tasks completed in this file.

---
+++ day_3/day_3.js
@@ -0,0 +1,39 @@
+// Task#1
+/*
+let num = '5';
+if(num>0){
+    console.log("Positive");
+
+}
+else if(num<0){
+    console.log("Nagetive")
+}
+else if(num===0){
+   console.log( "Zero")
+}
+else{
+    console.log("Not a valid number.")
+}
+    */
+
+//  Task#2
+
+age = 18;
+if(age>=18){
+    console.log("Eligible to vote.")
+}
+else("Wait kr haali")
+
+// Task#3
+let a=2;
+let b=4;
+let c=8;
+if(a>b && b>c){
+    console.log("A is greater");
+}
+else if(b>a && a>c){
+    console.log("b is greater");
+}
+else{
+    console.log ("c is greater")
+}
